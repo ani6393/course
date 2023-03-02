e3e7f88a6733d33dfedd85eaecc612f6c6f8a0de
Aniket Kumar <ani6393knp@gmail.com>
2023-03-02 14:13:16 +0530

    sdhhdu

---
+++ Image_api_2/src/main/java/com/image/ImageApi2Application.java
@@ -0,0 +1,13 @@
+package com.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageApi2Application.class, args);
+	}
+
+}
